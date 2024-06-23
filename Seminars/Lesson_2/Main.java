7e063a317d8a56bc6a21c58847cf593bc5dcc113
SergejMilovanov <milowanow-sergej@mail.ru>
2024-06-23 11:10:38 +0300

    Дополнил коды ко 2 семинару по ДЗ

--- Seminars/Lesson_2/Main.java
+++ Seminars/Lesson_2/Main.java
@@ -1,5 +1,18 @@
 package Seminars.Lesson_2;
 
 public class Main {
-    
+    public static void main(String[] args) {
+
+        Human hm = new Human("Sergey");
+
+        Market market = new Market();
+
+        market.acceptToMarket(hm);
+        market.takeInQueue(hm);
+        market.giveOrders();
+        market.takeOrders();
+        market.releaseFromQueue();
+        market.update();
+
+    }
 }
--- Seminars/Lesson_2/Market.java
+++ Seminars/Lesson_2/Market.java
@@ -1,50 +1,61 @@
 package Seminars.Lesson_2;
 
+import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Queue;
 
 public class Market implements MarketBehaviour, QueueBehaviour {
 
     private List<Actor> actors = new ArrayList<Actor>();
+    Queue<Actor> actorsQueue = new ArrayDeque<>();
+    private Actor actor;
 
     @Override
     public void acceptToMarket(Actor actor) {
         actors.add(actor);
+        System.out.println(actor.getName() + " посетил магазин");
     }
 
     @Override
     public void releaseFromMarket(List<Actor> actors) {
         actors.remove(0);
+        System.out.println(actor.getName() + " покинул магазин");
     }
 
     @Override
     public void update() {
-        // TODO Auto-generated method stub
-        
+        int ordersCount = 0;
+        for (Actor actor : actors) {
+            if (actor.isMakeOrder()) {
+                ordersCount++;
+            }
+        }
+        System.out.println("Количество заказов: " + ordersCount);
     }
 
     @Override
     public void giveOrders() {
-        // TODO Auto-generated method stub
-        
+        actorsQueue.peek().setMakeOrder(true);
+        System.out.println(actorsQueue.peek().getName() + " сделал заказ");
     }
 
     @Override
     public void releaseFromQueue() {
-        // TODO Auto-generated method stub
-        
+        System.out.println(actorsQueue.peek().getName() + " покинул очередь");
+        actorsQueue.poll();
     }
 
     @Override
     public void takeInQueue(Actor actor) {
-        // TODO Auto-generated method stub
-        
+        actorsQueue.add(actor);
+        System.out.println(actor.getName() + " встал в очередь");
     }
 
     @Override
     public void takeOrders() {
-        // TODO Auto-generated method stub
-        
+        actorsQueue.peek().setTakeOrder(true);
+        System.out.println(actorsQueue.peek().getName() + " забрал заказ");
     }
-    
+
 }
